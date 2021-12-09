83277e12cb9c009abe81282c2174328bc2f83691
witersen <1801168257@qq.com>
2021-12-09 10:28:01 +0800

    删除测试文件

--- 02.后端开发/client.php
+++
@@ -1,9 +0,0 @@
-<?php
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("error:" . socket_strerror(socket_last_error()));
-$msg = "---yes yes---";
-$len = strlen($msg);
-$server = socket_connect($socket, '127.0.0.1', 7777);
-socket_write($socket, $msg);
-$msg = socket_read($socket, 8192);
-print($msg);
-socket_close($socket);
