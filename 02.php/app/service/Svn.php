a818c62912141af58257c461d677aa1989ce50a3
witersen <1801168257@qq.com>
2022-12-15 22:39:48 +0800

    v2.4.4

--- 02.php/app/service/Svn.php
+++ 02.php/app/service/Svn.php
@@ -154,11 +154,9 @@ class Svn extends Base
             $ldap_bind_dn = $ldap['ldap_bind_dn'];
             $ldap_bind_pw = $ldap['ldap_bind_password'];
             $ldap_search_base = $ldap['user_base_dn'];
-            
+
             if (substr($ldap['user_search_filter'], 0, 1) == '(' && substr($ldap['user_search_filter'], -1) == ')') {
-                $ldap_filter = $ldap['user_search_filter'];
-            } else {
-                $ldap_filter = '(' . $ldap['user_search_filter'] . ')';
+                $ldap_filter = ltrim(rtrim($ldap['user_search_filter'], ')'), '(');
             }
 
             $ldap_filter =  '(&(' . explode(',', $ldap['user_attributes'])[0] . '=%U)(' . $ldap_filter . '))';
